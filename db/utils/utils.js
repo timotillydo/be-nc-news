23328f115aefff55748198f2a61b1b99f445eb97
timotillydo <44656173+timotillydo@users.noreply.github.com>
2019-08-25 13:50:53 +0100

    completed formatComments util function with TDD

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -21,3 +21,25 @@ exports.makeRefObj = (list, refKey, refValue) => {
   list.forEach(obj => (newRefObj[obj[refKey]] = obj[refValue]));
   return newRefObj;
 };
+
+exports.formatComments = (comments, articleRef) => {
+  if (!comments.length) return [];
+  const formattedComments = [];
+  comments.forEach(comment => {
+    const newComment = {};
+    for (let key in comment) {
+      if (key === "created_by") {
+        newComment.author = comment[key];
+        delete comment.created_by;
+      } else if (key === "belongs_to") {
+        newComment.article_id = articleRef[comment[key]];
+      } else if (key === "created_at") {
+        newComment.created_at = new Date(comment[key]);
+      } else {
+        newComment[key] = comment[key];
+      }
+    }
+    formattedComments.push(newComment);
+  });
+  return formattedComments;
+};
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -73,3 +73,102 @@ describe("makeRefObj", () => {
     expect(actualResult).to.eql(expectedResult);
   });
 });
+
+describe.only("formatComments", () => {
+  it("should return an empty array when passed an empty array", () => {
+    expect(formatComments([])).to.eql([]);
+  });
+  it("should return an array of one comment object with its 'created_by' key renamed to 'author'", () => {
+    const comments = [{ created_by: "butter_bridge" }];
+    const actualResult = formatComments(comments);
+    const expectedResult = [{ author: "butter_bridge" }];
+    expect(actualResult).to.eql(expectedResult);
+  });
+  it("should return an array of one comment object with it's 'belongs_to' key renamed to 'article_id' and it's value should be the corresponding id", () => {
+    const comments = [{ belongs_to: "They're not exactly dogs, are they?" }];
+    const refObj = { "They're not exactly dogs, are they?": 3 };
+    const actualResult = formatComments(comments, refObj);
+    const expectedResult = [{ article_id: 3 }];
+    expect(actualResult).to.eql(expectedResult);
+  });
+  it("should return an array with one comment object that has it's created_at value converted into a javascript date object", () => {
+    const comments = [{ created_at: 1511354163389 }];
+    const actualResult = formatComments(comments);
+    const expectedResult = [{ created_at: new Date(1511354163389) }];
+    expect(actualResult).to.eql(expectedResult);
+  });
+  it("should return an array of multiple formatted objects with all non-formatted keys maintained, when passed an array of multiple comment objects", () => {
+    const comments = [
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: "butter_bridge",
+        votes: 16,
+        created_at: 1511354163389
+      },
+      {
+        body:
+          "The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.",
+        belongs_to: "Living in the shadow of a great man",
+        created_by: "butter_bridge",
+        votes: 14,
+        created_at: 1479818163389
+      },
+      {
+        body:
+          "Replacing the quiet elegance of the dark suit and tie with the casual indifference of these muted earth tones is a form of fashion suicide, but, uh, call me crazy — onyou it works.",
+        belongs_to: "Living in the shadow of a great man",
+        created_by: "icellusedkars",
+        votes: 100,
+        created_at: 1448282163389
+      },
+      {
+        body: " I carry a log — yes. Is it funny to you? It is not to me.",
+        belongs_to: "Living in the shadow of a great man",
+        created_by: "icellusedkars",
+        votes: -100,
+        created_at: 1416746163389
+      }
+    ];
+    const refObj = {
+      "They're not exactly dogs, are they?": 3,
+      "Living in the shadow of a great man": 4
+    };
+    const actualResult = formatComments(comments, refObj);
+    const expectedResult = [
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        article_id: 3,
+        author: "butter_bridge",
+        votes: 16,
+        created_at: new Date(1511354163389)
+      },
+      {
+        body:
+          "The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.",
+        article_id: 4,
+        author: "butter_bridge",
+        votes: 14,
+        created_at: new Date(1479818163389)
+      },
+      {
+        body:
+          "Replacing the quiet elegance of the dark suit and tie with the casual indifference of these muted earth tones is a form of fashion suicide, but, uh, call me crazy — onyou it works.",
+        article_id: 4,
+        author: "icellusedkars",
+        votes: 100,
+        created_at: new Date(1448282163389)
+      },
+      {
+        body: " I carry a log — yes. Is it funny to you? It is not to me.",
+        article_id: 4,
+        author: "icellusedkars",
+        votes: -100,
+        created_at: new Date(1416746163389)
+      }
+    ];
+    expect(actualResult).to.eql(expectedResult);
+  });
+});
