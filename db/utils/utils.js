3b27277dcf2ac5ae5597da46189e48cfda43c6a6
timotillydo <44656173+timotillydo@users.noreply.github.com>
2019-08-25 13:46:18 +0100

    completed first util function to format dates

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,5 +1,16 @@
-exports.formatDates = list => {};
-
-exports.makeRefObj = list => {};
-
-exports.formatComments = (comments, articleRef) => {};
+exports.formatDates = list => {
+  if (!list.length) return [];
+  const formattedArray = [];
+  list.forEach(dateObj => {
+    const newDateObj = {};
+    for (let key in dateObj) {
+      if (key === "created_at") {
+        newDateObj.created_at = new Date(dateObj[key]);
+      } else {
+        newDateObj[key] = dateObj[key];
+      }
+    }
+    formattedArray.push(newDateObj);
+  });
+  return formattedArray;
+};
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,12 +1,46 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 const {
   formatDates,
   makeRefObj,
-  formatComments,
-} = require('../db/utils/utils');
+  formatComments
+} = require("../db/utils/utils");
 
-describe('formatDates', () => {});
-
-describe('makeRefObj', () => {});
-
-describe('formatComments', () => {});
+describe("formatDates", () => {
+  it("should return an array when passed an empty array", () => {
+    expect(formatDates([])).to.eql([]);
+  });
+  it("should return an array with a single object which has it's: created_at timestamp value formatted correctly when passed an array with one object which can have any unix timestamp as the value of the created_at key", () => {
+    let inputArray = [{ created_at: 0 }];
+    let actualResult = formatDates(inputArray);
+    expect(actualResult[0].created_at).to.eql(new Date(0));
+    inputArray = [{ created_at: 1542284514171 }];
+    actualResult = formatDates(inputArray);
+    expect(actualResult[0].created_at).to.eql(new Date(1542284514171));
+    inputArray = [{ created_at: 1416140514171 }];
+    actualResult = formatDates(inputArray);
+    expect(actualResult[0].created_at).to.eql(new Date(1416140514171));
+  });
+  it("should return an array of objects with each created_at key on each object formatted correctly when passed an array of multiple objects", () => {
+    const inputArray = [
+      { created_at: 1416140514171 },
+      { created_at: 1403143051 },
+      { created_at: 1203143051 },
+      { created_at: 1542284514171 }
+    ];
+    const actualResult = formatDates(inputArray);
+    const inputCopy = [
+      { created_at: 1416140514171 },
+      { created_at: 1403143051 },
+      { created_at: 1203143051 },
+      { created_at: 1542284514171 }
+    ];
+    const expectedResult = [
+      { created_at: new Date(1416140514171) },
+      { created_at: new Date(1403143051) },
+      { created_at: new Date(1203143051) },
+      { created_at: new Date(1542284514171) }
+    ];
+    expect(actualResult).to.eql(expectedResult);
+    expect(actualResult).to.not.equal(expectedResult);
+  });
+});
