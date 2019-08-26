5f1ef589ff845a88d9a67244fff540d28397b512
timotillydo <44656173+timotillydo@users.noreply.github.com>
2019-08-26 14:11:17 +0100

    error handling for invalid request path

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const apiRouter = require("./routes/api-router");
 
 app.use("/api", apiRouter);
 
+app.all("/*", (req, res, next) => {
+  res.status(404).send({ errMsg: "Error 404: Route Not Found" });
+});
+
 module.exports = app;
--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,7 +1,7 @@
 process.env.NODE_ENV = "test";
 
-const request = require("supertest");
 const app = require("../app");
+const request = require("supertest")(app);
 const chai = require("chai");
 const { expect } = chai;
 const connection = require("../db/connection");
@@ -10,16 +10,28 @@ describe("/api", () => {
   after(() => {
     connection.destroy();
   });
-  describe("/topics", () => {
-    describe("GET requests", () => {
+  describe("GET requests", () => {
+    describe("/topics", () => {
       it("returns a status code: 200 and an array of all topics", () => {
-        return request(app)
+        return request
           .get("/api/topics")
           .expect(200)
           .then(({ body: { topics } }) => {
             expect(topics).to.have.length(3);
+            expect(topics[0]).to.have.keys("slug", "description");
           });
       });
     });
+  });
+});
+
+describe("error handling request to invalid/unbuilt endpoint", () => {
+  it("returns status code: 404 and error message: Route Not Found", () => {
+    return request
+      .get("/api/tooooopics")
+      .expect(404)
+      .then(({ body: { errMsg } }) => {
+        expect(errMsg).to.equal("Error 404: Route Not Found");
+      });
   });
 });
